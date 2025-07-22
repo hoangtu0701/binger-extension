ff511a02e8273c334adb47393e4d052bed29cc9f
tungh <hoangtu3@msu.edu>
2025-07-22 11:45:38 +0700

    Updated

--- background.js
+++ background.js
@@ -135,12 +135,6 @@ try {
 
         console.log("[Binger] Persistent connection established with tab", tabId);
 
-        port.onMessage.addListener(msg => {
-            if (msg.type === "ping") {
-            // Do nothing, but it wakes the worker
-            }
-        });
-
         port.onDisconnect.addListener(() => {
             console.log("[Binger] Tab closed or reloaded — cleaning up");
 
@@ -1152,12 +1146,6 @@ try {
         }
 
         sendResponse({ status: "detached" });
-        return true;
-    }
-
-    if (msg.type === "ping") {
-        // Do nothing, but it wakes the worker
-        sendResponse();  
         return true;
     }
 
