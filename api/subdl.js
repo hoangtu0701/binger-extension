8c2ae3542d837a37c93ec26c9f6764a4ae87fcaf
tungh <hoangtu3@msu.edu>
2025-09-01 17:24:01 -0400

    updated

--- api/subdl.js
+++ api/subdl.js
@@ -1,5 +1,13 @@
 export default async function handler(req, res) {
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
+
     const { film_name } = req.query;
     if (!film_name) {
       return res.status(400).json({ error: "film_name is required" });
